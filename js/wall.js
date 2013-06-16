d4f7a5b699367e0747302802af2965bd0fdb7e6f
raulooo <erreauele@gmail.com>
2013-06-16 12:34:10 -0700

    last commit

--- js/wall.js
+++ js/wall.js
@@ -4,13 +4,6 @@ myapp.controller('WallCtrl', ['$scope', 'angularFireCollection',
   function($scope, angularFireCollection) {
     var url = 'https://cutra.firebaseio.com/apps';
     $scope.apps = angularFireCollection(url);
-    // var appx = $scope.apps;
-    // $scope.apps1 = [];
-    // console.log(appx);
-    // console.log(appx[0]);
-    // for (var i=0; i<$scope.apps.length || i<3; i++) {
-    //     $scope.apps1.push($scope.apps[i]);
-    // }
     
   }
 ]);
